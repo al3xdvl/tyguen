19e165a9aae3ab80c1d6ed64ff5dbfdb7b15ee74
Alex Deval <al3x.dvl@gmail.com>
2018-05-02 17:59:25 +0200

    début

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,33 @@
 //= require rails-ujs
 //= require_tree .
+
+//= require jquery
+//= require jquery_ujs
+
+//= require moment
+//= require fullcalendar
+//= require fullcalendar/gcal
+//= require fullcalendar/locale-all
+
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+       googleCalendarApiKey: 'AIzaSyCVZm2-uidFjJY8uS8iD20HocubVUSWZk4',
+       locale: 'fr',
+    events: {
+      googleCalendarId: 'tyguen@gmail.com',
+      color: 'black',
+      textColor: 'white',
+      rendering: 'background'
+    },
+    header: {
+      left:   'title',
+      center: '',
+      right:  'today prev,next nextYear'
+    },
+    buttonText: {
+      today:    "Aujourd'hui",
+    },
+    firstDay: 6,
+    weekNumbers: true
+    })
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get '/pages/dispo', to: 'pages#dispo', as: 'dispo'
+  get '/pages/contact', to: 'pages#contact', as: 'contact'
+  get 'messages', to: 'messages#new'
+  post 'messages', to: 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
