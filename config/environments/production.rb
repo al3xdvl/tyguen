79fc2c947935f335bf374750f9c6ab9484fe69aa
Alex Deval <al3x.dvl@gmail.com>
2018-06-05 14:14:57 +0200

    contact form amélioré

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,15 +68,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "tyguen.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.perform_caching = false
-  # config.action_mailer.delivery_method = :mailgun
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV['GMAIL_DOMAIN'],
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV['TYGUEN_EMAIL_ADDRESS'],
     password: ENV['TYGUEN_EMAIL_PASSWORD']
