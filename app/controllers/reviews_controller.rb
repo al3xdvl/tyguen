2debfac3191b4a39bc9d10582a51f56f21ebdde2
Alex Deval <al3x.dvl@gmail.com>
2018-06-05 10:42:01 +0200

    livre d'or modifié

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,6 +22,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content, :name, :date)
+    params.require(:review).permit(:content, :name, :date, :location)
   end
 end
