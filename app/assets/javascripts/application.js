432ac6c8e8cefe9557d5b19147a4c968e6d8632f
Alex Deval <al3x.dvl@gmail.com>
2018-06-26 19:21:22 +0200

    améliorations sarah + calendrier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,62 @@ $(document).ready(function() {
     $('#calendar').fullCalendar({
        googleCalendarApiKey: "AIzaSyCVZm2-uidFjJY8uS8iD20HocubVUSWZk4",
        locale: 'fr',
-    events: {
+    events:
+    {
       googleCalendarId: 'tyguen@gmail.com',
       color: '#0065A8',
       textColor: 'white',
-      className: ["background-events"]
+      className: ["background-events"],
     },
-    header: {
+      header: {
+      left:   'title',
+      center: ' ',
+      right:  'prevYear prev,next nextYear'
+    },
+    buttonText: {
+      today:    "Aujourd'hui",
+    },
+    firstDay: 6,
+    weekNumbers: true
+    })
+    $('#calendar2').fullCalendar({
+
+    eventSources: [
+
+    // your event source
+    {
+      events: [ // put the array in the `events` property
+        {
+            title : 'Zone C - 280 euros',
+      start : '2018-05-05',
+      end : '2018-06-30'
+        },
+        {
+            title : 'Zone C - 280 euros',
+      start : '2018-09-01',
+      end : '2018-09-29'
+        }
+      ],
+      color: '#00C21D',     // an option!
+      textColor: 'black' // an option!
+    },
+    {
+      events: [ // put the array in the `events` property
+        {
+      title : 'Zone E - 420 euros',
+      start : '2018-06-30',
+      end : '2018-07-14'
+        }
+      ],
+      color: '#FF5300',     // an option!
+      textColor: 'black' // an option!
+    },
+
+
+    // any other event sources...
+
+  ],
+      header: {
       left:   'title',
       center: ' ',
       right:  'prevYear prev,next nextYear'
